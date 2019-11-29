a4d4781f39c5249943288d119581bc48daf535c9
Manish Nagarkoti <manishngk99@gmail.com>
2019-11-29 11:32:18 +0545

    Initial commit

---
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.cameraapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.ResultReceiver;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.net.Authenticator;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imgView;
+    private Button btnOpenCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView = findViewById(R.id.imageView);
+        btnOpenCamera = findViewById(R.id.button_operCamera);
+
+        checkPermission();
+
+        btnOpenCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        loadCamera();
+    }
+
+    private void loadCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 0);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    public void checkPermission() {
+        //checks whether user has camera permission nor not.
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
+        }
+    }
+}
